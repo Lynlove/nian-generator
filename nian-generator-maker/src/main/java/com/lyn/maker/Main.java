871844571c7828d839f7fdf7d1cdef7b4580ee20
lyn <3228562873@qq.com>
2024-07-03 19:03:25 +0800

    从可移植性、功能完善、健壮性、可扩展性等多个维度对制作工具项目进行了优化

--- nian-generator-maker/src/main/java/com/lyn/maker/Main.java
+++ nian-generator-maker/src/main/java/com/lyn/maker/Main.java
@@ -2,13 +2,15 @@ package com.lyn.maker;
 
 //import com.lyn.maker.cli.CommandExecutor;
 
+import com.lyn.maker.generator.main.MainGenerator;
+import freemarker.template.TemplateException;
+
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
-        args = new String[]{"generate", "-l", "-a", "-o"};
-//        args = new String[]{"config"};
-//        args = new String[]{"list"};
-//        CommandExecutor commandExecutor = new CommandExecutor();
-//        commandExecutor.doExecute(args);
+    public static void main(String[] args) throws TemplateException, IOException, InterruptedException {
+        MainGenerator mainGenerator = new MainGenerator();
+        mainGenerator.doGenerate();
     }
 }
